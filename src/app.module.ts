5834424ca6036d33ccc85876f01a889d3dae0fa7
Tukhtakhodjaev Sardorkhuja <mr.khuzha@gmail.com>
2022-02-20 22:54:15 +0300

    add prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HostelsModule } from './hostels/hostels.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [HostelsModule],
+  imports: [HostelsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
