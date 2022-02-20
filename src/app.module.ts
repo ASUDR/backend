b00f58fd2ec6f48789a7fb1289420ae0f66036ac
Тухтаходжаев Сардорхужа Абдурахимович <SATukhtakhodzhaev@sberbank.ru>
2022-02-20 22:48:38 +0300

    add prisma, add hostel model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HostelsModule } from './hostels/hostels.module';
 
 @Module({
-  imports: [],
+  imports: [HostelsModule],
   controllers: [AppController],
   providers: [AppService],
 })
