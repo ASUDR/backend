49fddad4cdb0013410569dd178788c0abde224cd
Тухтаходжаев Сардорхужа Абдурахимович <SATukhtakhodzhaev@sberbank.ru>
2022-02-21 00:10:49 +0300

    add crud for floors

--- src/floors/floors.controller.ts
+++ src/floors/floors.controller.ts
@@ -1,7 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateFloorDto } from './dto/create-floor.dto';
+import { UpdateFloorDto } from './dto/update-floor.dto';
 import { FloorsService } from './floors.service';
 
 @Controller('floors')
 export class FloorsController {
   constructor(private readonly floorsService: FloorsService) {}
+
+  @Post()
+  async create(@Body() dto: CreateFloorDto) {
+    return this.floorsService.create(dto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.floorsService.findMany();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.floorsService.findOne(id);
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() dto: UpdateFloorDto) {
+    return this.floorsService.update(id, dto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    return this.floorsService.remove(id);
+  }
 }
--- src/floors/floors.module.ts
+++ src/floors/floors.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FloorsService } from './floors.service';
 import { FloorsController } from './floors.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [FloorsController],
   providers: [FloorsService],
 })
