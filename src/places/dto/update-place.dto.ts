f5acbbfc7c3502b7d283d116c23d897adad3c1ec
Тухтаходжаев Сардорхужа Абдурахимович <SATukhtakhodzhaev@sberbank.ru>
2022-02-21 01:31:52 +0300

    fix misspelling

--- src/places/dto/update-place.dto.ts
+++ src/places/dto/update-place.dto.ts
@@ -1,6 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { IsDefined, IsString } from 'class-validator';
-import { CreatePlaceDto } from './craete-place.dto';
+import { CreatePlaceDto } from './create-place.dto';
 
 export class UpdatePlaceDto extends PartialType(CreatePlaceDto) {
   @IsDefined()
