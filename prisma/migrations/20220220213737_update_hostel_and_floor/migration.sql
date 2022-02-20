24cc7e3df552402413caa220fc7f5c104d1d5044
Тухтаходжаев Сардорхужа Абдурахимович <SATukhtakhodzhaev@sberbank.ru>
2022-02-21 00:39:56 +0300

    update floor and hostel

--- prisma/migrations/20220220213737_update_hostel_and_floor/migration.sql
+++ prisma/migrations/20220220213737_update_hostel_and_floor/migration.sql
@@ -1,11 +1,22 @@
 -- CreateTable
+CREATE TABLE "Hostel" (
+    "id" TEXT NOT NULL DEFAULT public.uuid_generate_v4(),
+    "name" VARCHAR(50) NOT NULL,
+
+    CONSTRAINT "Hostel_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Floor" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
+    "id" TEXT NOT NULL DEFAULT public.uuid_generate_v4(),
+    "name" VARCHAR(50) NOT NULL,
     "hostelId" TEXT NOT NULL,
 
     CONSTRAINT "Floor_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Hostel_name_key" ON "Hostel"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Floor_name_key" ON "Floor"("name");
