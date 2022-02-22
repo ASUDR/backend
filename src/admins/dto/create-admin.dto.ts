1d74c70e8b5fab8462f8aa3378ceb50d8e3072f5
Tukhtakhodjaev Sardorkhuja <mr.khuzha@gmail.com>
2022-02-23 00:15:15 +0300

    add docs for create admin dto

--- src/admins/dto/create-admin.dto.ts
+++ src/admins/dto/create-admin.dto.ts
@@ -1,24 +1,36 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class CreateAdminDto {
+  @ApiProperty({
+    maxLength: 50,
+  })
   @IsString()
   @IsNotEmpty()
   @MaxLength(50)
   firstName: string;
 
+  @ApiProperty({
+    maxLength: 50,
+  })
   @IsString()
   @IsNotEmpty()
   @MaxLength(50)
   lastName: string;
 
+  @ApiProperty({
+    maxLength: 50,
+  })
   @IsOptional()
   @IsString()
   @MaxLength(50)
   patronymic?: string;
 
+  @ApiProperty()
   @IsString()
   roleId: string;
 
+  @ApiProperty()
   @IsString({ each: true })
   facultiesIds: Array<string>;
 }
