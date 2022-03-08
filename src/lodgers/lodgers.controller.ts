81e974f5f59e216126766c33f00d02caf87d9f4a
Tukhtakhodjaev Sardorkhuja <mr.khuzha@gmail.com>
2022-03-09 00:42:35 +0300

    add swagger to lodgers

--- src/lodgers/lodgers.controller.ts
+++ src/lodgers/lodgers.controller.ts
@@ -3,44 +3,54 @@ import {
   Controller,
   Delete,
   Get,
+  HttpStatus,
   Param,
   Patch,
   Post,
   UseGuards,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CreateLodgerDto } from './dto/create-lodger.dto';
 import { UpdateLodgerDto } from './dto/update-lodger.dto';
+import { Lodger } from './entities/lodger.entity';
 import { LodgersService } from './lodgers.service';
 
 @ApiTags('lodgers')
 @Controller('lodgers')
-@UseGuards(JwtAuthGuard)
+// @UseGuards(JwtAuthGuard)
 export class LodgersController {
   constructor(private readonly lodgersService: LodgersService) {}
 
   @Post()
-  async create(@Body() dto: CreateLodgerDto) {
+  @ApiResponse({ status: HttpStatus.CREATED, type: Lodger })
+  async create(@Body() dto: CreateLodgerDto): Promise<Lodger> {
     return this.lodgersService.create(dto);
   }
 
   @Get()
+  @ApiResponse({ status: HttpStatus.OK, type: [Lodger] })
   async findAll() {
     return this.lodgersService.findMany();
   }
 
   @Get(':id')
+  @ApiResponse({ status: HttpStatus.OK, type: Lodger })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND })
   async findOne(@Param('id') id: string) {
     return this.lodgersService.findOne({ id: +id });
   }
 
   @Patch(':id')
+  @ApiResponse({ status: HttpStatus.OK, type: Lodger })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND })
   async update(@Param('id') id: string, @Body() dto: UpdateLodgerDto) {
     return this.lodgersService.update(+id, dto);
   }
 
   @Delete(':id')
+  @ApiResponse({ status: HttpStatus.OK, type: Lodger })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND })
   async remove(@Param('id') id: string) {
     return this.lodgersService.remove(+id);
   }
