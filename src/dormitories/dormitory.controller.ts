7fe42d06b8d222d07610f5faa5b973c8ed60f57d
Tukhtakhodjaev Sardorkhuja <mr.khuzha@gmail.com>
2022-03-10 01:42:07 +0300

    add swagger to some controllers

--- src/dormitories/dormitory.controller.ts
+++ src/dormitories/dormitory.controller.ts
@@ -7,41 +7,56 @@ import {
   Param,
   Delete,
   UseGuards,
+  HttpStatus,
 } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { HostelsService } from './dormitory.service';
 import { CreateHostelDto } from './dto/create-hostel.dto';
 import { UpdateHostelDto } from './dto/update-hostel.dto';
+import { Dormitory } from './entities/dormitory.entity';
 
-@ApiTags('hostels')
+@ApiTags('dormitories')
 @Controller('hostels')
 // @UseGuards(JwtAuthGuard)
 export class HostelsController {
   constructor(private readonly hostelsService: HostelsService) {}
 
   @Post()
-  async create(@Body() dto: CreateHostelDto) {
+  @ApiResponse({ status: HttpStatus.CREATED, type: Dormitory })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR })
+  async create(@Body() dto: CreateHostelDto): Promise<Dormitory> {
     return this.hostelsService.create(dto);
   }
 
   @Get()
-  async findAll() {
+  @ApiResponse({ status: HttpStatus.OK, type: [Dormitory] })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR })
+  async findAll(): Promise<Array<Dormitory>> {
     return this.hostelsService.findMany();
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string) {
+  @ApiResponse({ status: HttpStatus.OK, type: Dormitory })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND })
+  async findOne(@Param('id') id: string): Promise<Dormitory> {
     return this.hostelsService.findOne({ id: +id });
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() dto: UpdateHostelDto) {
+  @ApiResponse({ status: HttpStatus.OK, type: Dormitory })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND })
+  async update(@Param('id') id: string, @Body() dto: UpdateHostelDto): Promise<Dormitory> {
     return this.hostelsService.update(+id, dto);
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string) {
+  @ApiResponse({ status: HttpStatus.OK, type: Dormitory })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND })
+  async remove(@Param('id') id: string): Promise<Dormitory> {
     return this.hostelsService.remove(+id);
   }
 }
--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -3,45 +3,60 @@ import {
   Controller,
   Delete,
   Get,
+  HttpStatus,
   Param,
   Patch,
   Post,
   UseGuards,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CreateGroupDto } from './dto/create-group.dto';
 import { UpdateGroupDto } from './dto/update-group.dto';
+import { Group } from './entities/group.entity';
 import { GroupsService } from './groups.service';
 
 @ApiTags('groups')
 @Controller('groups')
-@UseGuards(JwtAuthGuard)
+// @UseGuards(JwtAuthGuard)
 export class GroupsController {
   constructor(private readonly groupsService: GroupsService) {}
 
   @Post()
-  async create(@Body() dto: CreateGroupDto) {
+  @ApiResponse({ status: HttpStatus.CREATED, type: Group })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR })
+  async create(@Body() dto: CreateGroupDto): Promise<Group> {
     return this.groupsService.create(dto);
   }
 
   @Get()
-  async findAll() {
+  @ApiResponse({ status: HttpStatus.OK, type: [Group] })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR })
+  async findAll(): Promise<Array<Group>> {
     return this.groupsService.findMany();
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string) {
+  @ApiResponse({ status: HttpStatus.OK, type: Group })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND })
+  async findOne(@Param('id') id: string): Promise<Group> {
     return this.groupsService.findOne({ id: +id });
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() dto: UpdateGroupDto) {
+  @ApiResponse({ status: HttpStatus.OK, type: Group })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND })
+  async update(@Param('id') id: string, @Body() dto: UpdateGroupDto): Promise<Group> {
     return this.groupsService.update(+id, dto);
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string) {
+  @ApiResponse({ status: HttpStatus.OK, type: Group })
+  @ApiResponse({ status: HttpStatus.INTERNAL_SERVER_ERROR })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND })
+  async remove(@Param('id') id: string): Promise<Group> {
     return this.groupsService.remove(+id);
   }
 }
